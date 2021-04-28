7ba00ba291104f2bd20629f35412b6a5df08c1fd
juan-salmeron11 <47012819+juan-salmeron11@users.noreply.github.com>
2021-04-28 16:53:08 -0500

    FOREST GAME

--- Final_Game.c
+++ Final_Game.c
@@ -148,8 +148,141 @@ const unsigned char name[]={\
 /********************************* META SPRITES FOR FRUIT GAME***************************************************/
 
 
+// define a 2x2 metasprite
+#define DEF_METASPRITE_CHAR(name,code,pal)\
+const unsigned char name[]={\
+        0,      0,      (code)+0,   pal, \
+        0,      8,      (code)+1,   pal, \
+        8,      0,      (code)+2,   pal, \
+        8,      8,      (code)+3,   pal, \
+        128};
+
+// define a 2x2 metasprite, flipped horizontally
+#define DEF_METASPRITE_CHAR_FLIP(name,code,pal)\
+const unsigned char name[]={\
+        8,      0,      (code)+0,   (pal)|OAM_FLIP_H, \
+        8,      8,      (code)+1,   (pal)|OAM_FLIP_H, \
+        0,      0,      (code)+2,   (pal)|OAM_FLIP_H, \
+        0,      8,      (code)+3,   (pal)|OAM_FLIP_H, \
+        128};
+
+//Metasprite for bear enemy
+#define DEF_METASPRITE_B_2x2(name,code,pal)\
+const unsigned char name[]={\
+        0,      0,      (code)+0,   pal, \
+        0,      8,      (code)+1,   pal, \
+        8,      0,      (code)+2,   pal, \
+        8,      8,      (code)+3,   pal, \
+        128};
+
+// define a 2x2 metasprite, flipped horizontally
+#define DEF_METASPRITE_B_2x2_FLIP(name,code,pal)\
+const unsigned char name[]={\
+        8,      0,      (code)+0,   (pal)|OAM_FLIP_H, \
+        8,      8,      (code)+1,   (pal)|OAM_FLIP_H, \
+        0,      0,      (code)+2,   (pal)|OAM_FLIP_H, \
+        0,      8,      (code)+3,   (pal)|OAM_FLIP_H, \
+        128};
 
 
+
+
+/*Metasprite for bird enemy***************************COPY HERE (A) ************************/
+
+#define DEF_METASPRITE_C_2x2(name,code,pal)\
+const unsigned char name[]={\
+        0,      0,      (code)+0,   pal, \
+        0,      8,      (code)+1,   pal, \
+        8,      0,      (code)+2,   pal, \
+        8,      8,      (code)+3,   pal, \
+        128};
+
+
+
+
+/********************************/
+
+
+
+
+
+
+
+//Metasprite for bear
+DEF_METASPRITE_B_2x2(bearRStand, 0xc4, 0);
+DEF_METASPRITE_B_2x2(bearRRun1, 0xc4, 0);
+DEF_METASPRITE_B_2x2(bearRRun2, 0xc8, 0);
+DEF_METASPRITE_B_2x2(bearRRun3, 0xcc, 0);
+
+//Flipped bear Metasprites
+DEF_METASPRITE_B_2x2_FLIP(bearLStand, 0xd8, 0);
+DEF_METASPRITE_B_2x2_FLIP(bearLRun1, 0xdc, 0);
+DEF_METASPRITE_B_2x2_FLIP(bearLRun2, 0xe0, 0);
+DEF_METASPRITE_B_2x2_FLIP(bearLRun3, 0xe4, 0);
+
+
+
+/****Metasprite for bird******************************COPY HERE (B) ************/
+
+DEF_METASPRITE_C_2x2(birdStand, 0xf0, 0);
+DEF_METASPRITE_C_2x2(birdFly1, 0xf0, 0);
+DEF_METASPRITE_C_2x2(birdFly2, 0xf4, 0);
+DEF_METASPRITE_C_2x2(birdFly3, 0xf8, 0);
+
+/*****************************************************COPY HERE (B) ************/
+
+
+
+
+//Meta sprite for player 
+DEF_METASPRITE_CHAR(layerRStand, 0xd8, 0);
+DEF_METASPRITE_CHAR(layerRRun1, 0xdc, 0);
+DEF_METASPRITE_CHAR(layerRRun2, 0xe0, 0);
+DEF_METASPRITE_CHAR(layerRRun3, 0xe4, 0);
+
+DEF_METASPRITE_CHAR_FLIP(layerLStand, 0xd8, 0);
+DEF_METASPRITE_CHAR_FLIP(layerLRun1, 0xdc, 0);
+DEF_METASPRITE_CHAR_FLIP(layerLRun2, 0xe0, 0);
+DEF_METASPRITE_CHAR_FLIP(layerLRun3, 0xe4, 0);
+
+//Bear movement sequence
+const unsigned char* const bearRunSeq[16] = {
+  bearLRun1, bearLRun2, bearLRun3, 
+  bearLRun1, bearLRun2, bearLRun3, 
+  bearLRun1, bearLRun2,
+  bearRRun1, bearRRun2, bearRRun3, 
+  bearRRun1, bearRRun2, bearRRun3, 
+  bearRRun1, bearRRun2,
+};
+
+
+
+
+/*****Bird movement sequence********************************* COPY HERE (C) **********/
+const unsigned char* const birdFlySeq[16] = {
+  birdFly1, birdFly1, birdFly1, 
+  birdFly1, birdFly1, birdFly2, 
+  birdFly2, birdFly2,
+  birdFly2, birdFly3, birdFly3, 
+  birdFly3, birdFly3, birdFly2, 
+  birdFly2, birdFly2,
+};
+/**************************************************************COPY HERE (C) ***********/
+
+
+
+//Player movement sequence
+const unsigned char* const layerRunSeq[16] = {
+  layerLRun1, layerLRun2, layerLRun3, 
+  layerLRun1, layerLRun2, layerLRun3, 
+  layerLRun1, layerLRun2,
+  layerRRun1, layerRRun2, layerRRun3, 
+  layerRRun1, layerRRun2, layerRRun3, 
+  layerRRun1, layerRRun2,
+};
+
+
+/*******************************/
 /********************************** MOTORCYCLE METASPRITE ANIMATION SEQUENCES **********************************************************************/
 
 
@@ -315,7 +448,50 @@ sbyte croc_dy;
 
 
 
+///////fruit VARIABLES
 
+// number of actors (1 per interactive Object)
+#define NUM_ACTORS 1
+#define NUM_FRUITS 5
+
+// actor x/y positions and speed
+
+
+// enemy BEAR actor
+byte enemy_x;
+byte enemy_y;
+// actor x/y deltas per frame (signed)
+sbyte enemy_dx;
+sbyte enemy_dy;
+
+
+
+/**** enemy BIRD actor****************************** COPY HERE (D) ************************/
+
+byte enemyBird_x;
+byte enemyBird_y;
+sbyte enemyBird_dx;
+sbyte enemyBird_dy;
+
+/********************r****************************** COPY HERE (D) ************************/
+
+
+
+typedef struct Fruit{
+  bool falling;
+  byte _x;		// fruit x/y position
+  byte _y;		
+  sbyte _dx;		// frui x/y deltas per frame (signed)
+  sbyte _dy;
+  int sprite;
+  int points;
+  
+};
+
+struct Fruit Fruits[4];
+
+
+////////
 
 //Boat has 3 lives
 int lives = 3;
@@ -348,6 +524,7 @@ int j;
 int arrow_x =25;
 int arrow_y =70;
 bool cleared[4];
+int score;
 
 /*{pal:"nes",layout:"nes"}*/
 const char PALETTE[32] = { 
@@ -1080,15 +1257,215 @@ void city(){
   
 }
 
-
+char pad2;
 void forest(){
   delay(60);
-    show_screen_scrolling(fruit_background_pal, fruit_background_rle,fruit_background_rle);
+   show_screen_scrolling(fruit_background_pal, fruit_background_rle,fruit_background_rle);
 
+  score =0;
+  lives = 3;
+  
+  
+  // Initialize actor fruits
+  for(i=0;i<4;i++){		
+    Fruits[i].falling = false;		//Controls when fruit falls
+    Fruits[i]._x = rndint(20,230);	//X position
+    Fruits[i]._y = rndint(15,70);	//Y position
+    Fruits[i]._dy = 0;			//Falling Speed
+    Fruits[i].sprite = i+19;		//Sprite used
+    Fruits[i].points = i+1;		//Points added when collected
+  }
+  
+  
+  //add_grass_tiles();
+  setup_graphics();
+//  famitone_init(effects);
+//  sfx_init(effects);
+//  nmi_set_callback(famitone_update);
+  
+  //Place the player in the middle of the screen
+    actor_x[0] = 120;
+    actor_y[0] = 191;
+    actor_dx[0] = 0;
+    actor_dy[0] = 0;
+  
+  
+  //Bear enemy on the corner of the screen
+    enemy_x = 0;
+    enemy_y = 100;
+    enemy_dx = 2;
+    enemy_dy = 0;
 
+/***************************************************** COPY HERE (E) *****************/
+  //Bird enemy on Right corner of the screen 
+    enemyBird_x = 250;
+    enemyBird_y = 161;
+    enemyBird_dx = -3;	//Delta values are placeholder, they need Sin(x) movement
+    enemyBird_dy = 1;
+  
+/***************************************************** COPY HERE (E) *****************/
 
+  
+  // Initiate Game loop
+  
+  famitone_init(danger_streets_music_data);
+  // set music callback function for NMI
+  nmi_set_callback(famitone_update);
+  // play music
+  music_play(0);
+  
+  while (1) {
+    // start with OAMid/sprite 0
+    oam_id = 0;
+    
+    // set player 0/1 velocity based on controller
+    for (i=0; i<1; i++) {
+      // poll controller i (0-1)
+      pad2 = pad_trigger(i);
+      pad = pad_poll(i);
+      
+      if (pad&PAD_LEFT && actor_x[i]>0) actor_dx[i]=-2;		//Moves player to the left until hits screen border
+      else if (pad&PAD_RIGHT && actor_x[i]<240) actor_dx[i]=2;	//Moves player to the right until hits screen border
+      else actor_dx[i]=0;					//Else horizontal acceleration = 0
+      
+      if (pad2 & PAD_A &&  actor_y[i] == 191)			//Prototype jumping
+      { 
+        actor_dy[i]=-2;
+      }
+            
+      
+    }
+    //Fall after Jumping to certain height;
+    if (actor_y[0] == 155)
+        actor_dy[0] = 2;
+    
+    //Drawing Player character
+    for (i=0; i<NUM_ACTORS; i++) {
+      byte runseq = actor_x[i] & 7;
+      if (actor_dx[0] >= 0)
+        runseq += 8;
+      oam_id = oam_meta_spr(actor_x[i], actor_y[i], oam_id, layerRunSeq[runseq]);
+      actor_x[i] += actor_dx[i];
+      //Protoype for jumping
+      if(actor_y[i] <= 191)
+      actor_y[i] += actor_dy[i];
+      //Set actor back on Plane after jumping if he falls too far
+      if(actor_y[i] >= 191)
+        actor_y[i] = 191;
+     
+    }
+    
+    //Drawing BEAR enemy
+    if(score > 10){					
+      enemy_y=191;
+    for (i=0; i<1; i++) {
+      byte runseq = enemy_x & 7;
+      if (enemy_dx >= 0)
+        runseq += 8;
+      oam_id = oam_meta_spr(enemy_x, enemy_y, oam_id, bearRunSeq[runseq]);
+      enemy_x += enemy_dx;
+    }
+    }
+    
+    
+/***************************************************** COPY HERE (F) *****************/
+    //Drawing BIRD enemy, after score is past 20pts
+    
+    if(score >= 0){
+      
+    for (i=0; i<1; i++) {
+     
+      
+      byte runseq = enemyBird_x & 7; 
+      if(enemyBird_y == 200)
+        enemyBird_dy = -1;
+      if(enemyBird_y == 150)
+        enemyBird_dy = 1;
+      if (enemyBird_dx >= 0)
+        runseq += 8;
+      oam_id = oam_meta_spr(enemyBird_x, enemyBird_y, oam_id, birdFlySeq[runseq]);
+      enemyBird_x += enemyBird_dx + 1;
+      enemyBird_y += enemyBird_dy;
+      
+      
+    }
+    }
+/***************************************************** COPY HERE (F) *****************/
+    
+    
+    
+    //Draws and updates hearts for lives
+    for(i=0;i<lives;i++)
+      oam_id = oam_spr(10+(i*10), 10, 23, 1, oam_id);
 
+    //Draws and updates Scoreboard
+    oam_id = oam_spr(232, 10, (score/10%10)+48, 2, oam_id);
+    oam_id = oam_spr(240, 10, (score%10)+48, 2, oam_id);
+    
+    for(i = 0; i<4; i++)
+     if(Fruits[i].sprite==20)
+     oam_id = oam_spr(Fruits[i]._x, Fruits[i]._y, Fruits[i].sprite, 2, oam_id);
+    else
+      oam_id = oam_spr(Fruits[i]._x, Fruits[i]._y, Fruits[i].sprite, 1, oam_id);
+      
+
+    for(i=0;i<4;i++){
+      if(rndint(1,200)==1)		//Fruit Hangs on tree for random set of rime
+        Fruits[i].falling = true;
+      
+      if(Fruits[i].falling)		//Set Fruit Fall speed 
+      	Fruits[i]._dy = rndint(1,3);
+        
+      Fruits[i]._y += Fruits[i]._dy;	//Make Fruit Fall
+      fruit_collision(i);		// Check Collsion with Player
+    }	
+    
+    //Enemy bear collision
+      if(enemy_y >= 210 || ((enemy_x >= actor_x[0]-4 && enemy_x <= actor_x[0]+8)&& (enemy_y >= actor_y[0]-2 && enemy_y <= actor_y[0]+4))){
+       lives--;
+       sfx_play(2,0);
+       enemy_x = 0;
+       delay(20);
+      }
+    
+
+/***************************************************** COPY HERE (G) *****************/
+    //Enemy Bird collision
+      if(enemyBird_y >= 210 || ((enemyBird_x >= actor_x[0]-4 && enemyBird_x <= actor_x[0]+8)&& (enemyBird_y >= actor_y[0]-2 && enemyBird_y <= actor_y[0]+4))){
+       lives--;
+       sfx_play(2,0);
+       enemyBird_x = 250;
+        delay(20);
+      }
+    
+/***************************************************** COPY HERE (G) *****************/    
+    
+    // hide rest of sprites
+    // if we haven't wrapped oam_id around to 0
+    if (oam_id!=0) oam_hide_rest(oam_id);
+    // wait for next frame
+    ppu_wait_frame();
+
+    
+    	//Win Conditions
+      if (score >= 50){
+      sfx_play(0,0);
+//victory();
+      break;
+      }
+    
+      //Lose Conditions
+      if (lives == 0){
+      sfx_play(0,0);
+      //gameOver();
+      break;
+    }
+  }
+  while(1){}
 }
+
+
+
 
 void show_screen(const byte* pal, const byte* rle,const byte* rle2) {
   // disable rendering
@@ -1113,4 +1490,44 @@ byte rndint(byte a, byte b){
 /*******************************************/
 
 
+void fruit_collision(int f){
+  if(Fruits[f]._y >= 210 || ((Fruits[f]._x >= actor_x[0]-4 && Fruits[f]._x <= actor_x[0]+8)&& (Fruits[f]._y >= actor_y[0]-2 && Fruits[f]._y <= actor_y[0]+4))) //hits floor or collision detected
+      {
+        if(Fruits[f]._y < 195){
+          sfx_play(1,1);
+          score += Fruits[f].points;
+          Fruits[f]._y = 210;
+        }
+        Fruits[f].sprite=0; // erase fruit for a bit
+      }
+  
+  if(Fruits[f].sprite == 0 && Fruits[f]._y <= 160 && Fruits[f]._y >= 130 ){// Make fruit reappear on random spot
+       	Fruits[f]._x = rndint(20,230);
+    	Fruits[f]._y = rndint(10,50);
+        Fruits[f]._dy = 0;
+    
+    //set sprite before reappearing
+    switch(f){
+      case 0:
+        Fruits[f].sprite=19;
+        Fruits[f].falling=false;
+        break;
+      case 1:
+        Fruits[f].sprite=20;
+        Fruits[f].falling=false;
+        break;
+      case 2:
+        Fruits[f].sprite=21;
+        Fruits[f].falling=false;
+        break;
+      case 3:
+        Fruits[f].sprite= 22;
+        Fruits[f].falling=false;
+        break;
+      default:
+        break;
+ 	}
+  }
+  
 
+}
